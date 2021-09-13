371102f25de571b5fdefb49b3015c245b44701da
rkrule <Robert.Krule@clarivate.com>
2021-09-13 11:00:01 -0400

    Update ClientBuilder.php
    Added ability to specify SOAP Endpoint URL in config for custom SFDC environments.

--- src/Tnw/SoapClient/ClientBuilder.php
+++ src/Tnw/SoapClient/ClientBuilder.php
@@ -18,14 +18,16 @@ class ClientBuilder
      * Construct client builder with required parameters
      *
      * @param string $wsdl        Path to your Salesforce WSDL
+     * @param string $location    Path to SFDC Endpoint Location
      * @param string $username    Your Salesforce username
      * @param string $password    Your Salesforce password
      * @param string $token       Your Salesforce security token
      * @param array  $soapOptions Further options to be passed to the SoapClient
      */
-    public function __construct($wsdl, $username, $password, $token, array $soapOptions = array())
+    public function __construct($wsdl, $location, $username, $password, $token, array $soapOptions = array())
     {
         $this->wsdl = $wsdl;
+        $this->location = $location;
         $this->username = $username;
         $this->password = $password;
         $this->token = $token;
@@ -56,6 +58,11 @@ class ClientBuilder
         $soapClientFactory = new SoapClientFactory();
         $soapClient = $soapClientFactory->factory($this->wsdl, $this->soapOptions);
 
+        if($this->location)
+        {
+            $soapClient->__setLocation($this->location);
+        }
+        
         $client = new Client($soapClient, $this->username, $this->password, $this->token);
 
         if ($this->log) {
